32b44fdc6b35aaefb08a4980434079c1e1036dd6
5mohamedasker <5mohamedasker@gmail.com>
2026-08-21 15:29:18 +0300

    Question02

--- OOPAssignment05/Program.cs
+++ OOPAssignment05/Program.cs
@@ -22,6 +22,26 @@
                                             so changes on one reference affect on the ather one
              */
             #endregion
+            #region Question02
+            //Q2 Shallow Copy vs Deep Copy
+            /*a) What is a Shallow Copy?
+                  ==>> We copy value types and with reference types we only copy the reference not the objects.
+            */
+            /*b) What is a Deep Copy?
+                  ==>> we copy value types and with reference types we teke a new copy of the object itself
+                       So there is no relationship between the old and the new.           
+            */
+            /*c) What happens to reference-type members when a Shallow Copy is created?
+                  ==>> We take a copy of the reference only.
+            */
+            /*d) What happens to reference-type members when a Deep Copy is created?
+             *    ==>> we teke a new copy of the object itself not the reference.
+            */
+            /*e) Give one situation where Deep Copy would be safer than Shallow Copy.
+                    If I have anything (Car version) and I want to make some modifications
+                    but I don't want the modifications to affect the original version.
+            */
+            #endregion
         }
     }
 }
